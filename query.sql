433ab71397710baef23ceb9f6ec2f06a5ca99236
Danni Hu <137489806+dannihuyang@users.noreply.github.com>
2024-06-20 22:52:27 -0700

    changed query 5 with step 1 and 2 in query.sql

--- query.sql
+++ query.sql
@@ -122,21 +122,71 @@ ORDER BY average_duration DESC;
 
 
 /*
-5. (Functional Needs - Intervention Effectiveness) 
-* Since functional_need’s relationship with incident_need still need other implementations, 
-this query can be currently used as a hypothetical analysis.
-* We can think of functional need for now as a need that each help_seeker might have but not necessarily addressed by the volunteers 
-(the volunteers do not have such resource) to exemplify how these needs in our current environment are unseen.
+Query 5:  On Intervention Effectiveness if with Functional Needs
 
-Are there any correlations between what the help seeker’s functional needs are and whether the interventions are successfully 
-resolved or not within a long enough period of time (12 months)? 
+Since functional_need’s relationship with incident_need still need other implementations, 
+this query can be currently used as a hypothetical analysis. 
+We currently think of functional need for now as a need that each help_seeker might have but not necessarily addressed by 
+the volunteers (the volunteers do not have such resource) to exemplify how these needs in our current environment are unseen.
+
+1) step 1: Are there any connection between help seeker having functional needs or not and whether the interventions 
+are successfully resolved or not within a long enough period of time (6months and 12 months)? 
+*/
+
+WITH EffectiveWithFunctionalNeed AS (
+	SELECT 
+		ROUND(100.0 * SUM(CASE WHEN intervention_status_name = 'Effective' AND DATEDIFF(intervention_end_date, intervention_start_date) <= 180 THEN 1 ELSE 0 END) 
+		/ COUNT(CASE WHEN intervention_status_name != 'Closed'
+			AND intervention_status_name != 'Escalated' THEN 1 END), 1) AS effective_6_months_with_fn,
+		ROUND(100.0 * SUM(CASE WHEN intervention_status_name = 'Effective' AND DATEDIFF(intervention_end_date, intervention_start_date) <= 365 THEN 1 ELSE 0 END) 
+		/ COUNT(CASE WHEN intervention_status_name != 'Closed'
+			AND intervention_status_name != 'Escalated' THEN 1 END), 1) AS effective_12_months_with_fn
+	FROM help_seeker
+	JOIN incident USING (help_seeker_id)
+	JOIN incident_need_list USING (incident_id)
+	JOIN intervention USING (incident_need_id)
+	JOIN intervention_status USING (intervention_status_id)
+	WHERE help_seeker_id IN
+		(SELECT DISTINCT
+			help_seeker_id
+		FROM help_seeker_functional_need)
+),
+EffectiveWithoutFunctionalNeed AS (
+	SELECT 
+		ROUND(100.0 * SUM(CASE WHEN intervention_status_name = 'Effective' AND DATEDIFF(intervention_end_date, intervention_start_date) <= 180 THEN 1 ELSE 0 END) 
+		/ COUNT(CASE WHEN intervention_status_name != 'Closed'
+			AND intervention_status_name != 'Escalated' THEN 1 END), 1) AS effective_6_months_without_fn,
+		ROUND(100.0 * SUM(CASE WHEN intervention_status_name = 'Effective' AND DATEDIFF(intervention_end_date, intervention_start_date) <= 365 THEN 1 ELSE 0 END) 
+		/ COUNT(CASE WHEN intervention_status_name != 'Closed'
+			AND intervention_status_name != 'Escalated' THEN 1 END), 1) AS effective_12_months_without_fn
+	FROM help_seeker
+	JOIN incident USING (help_seeker_id)
+	JOIN incident_need_list USING (incident_id)
+	JOIN intervention USING (incident_need_id)
+	JOIN intervention_status USING (intervention_status_id)
+		WHERE help_seeker_id NOT IN
+		(SELECT DISTINCT
+			help_seeker_id
+		FROM help_seeker_functional_need)
+)
+SELECT
+	(SELECT effective_6_months_with_fn FROM EffectiveWithFunctionalNeed) AS 6_months_with_fn,
+	(SELECT effective_6_months_without_fn FROM EffectiveWithoutFunctionalNeed) AS 6_months_without_fn,
+    (SELECT effective_12_months_with_fn FROM EffectiveWithFunctionalNeed) AS 12_months_with_fn,
+    (SELECT effective_12_months_without_fn FROM EffectiveWithoutFunctionalNeed) AS 12_months_without_fn;
+	 
+    
+/* step 2 
+Are there any correlations between what the help seeker’s functional needs are and whether the interventions
+are successfully resolved or not within a long enough period of time (12 months)? 
 For each kind of functional need of help seekers, what are the percentage of the interventions being effective, 
 order from least percentage of effectiveness to most.
 */
 SELECT 
 	functional_need_name,
  	ROUND(100.0 * SUM(CASE WHEN intervention_status_name = 'Effective' AND DATEDIFF(intervention_end_date, intervention_start_date) <= 365 THEN 1 ELSE 0 END) 
-    	/ COUNT(CASE WHEN intervention_status_name != 'Closed' THEN 1 END), 1) as effective_12_months
+		/ COUNT(CASE WHEN intervention_status_name != 'Closed'
+			AND intervention_status_name != 'Escalated' THEN 1 END), 1) AS effective_12_months
 FROM functional_need
 JOIN help_seeker_functional_need USING (functional_need_id)
 JOIN help_seeker USING (help_seeker_id)
